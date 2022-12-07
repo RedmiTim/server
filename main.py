0cb67ca62a3b534da9773fd9780b2907ad6e9c40
RedmiTim <timurkhasanov25122008@gmail.com>
2022-12-07 21:15:32 +0500

    first commit

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ l=random.randint(1,1000000)
 b=str(l)
 d=input('Введите никнейм')
 s=storage.create_user(d,b)
-#я ацтек
+import flask
+app = flask.Flask(__name__)
+def user():
+    name = flask.request.args['a']
+    k=storage.create_user(name)
+    return 'Вы передали: ' + name
+app.add_url_rule('/user', view_func=user)
+app.run(debug=True)
