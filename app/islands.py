fb7d3f857841b9f0d56697a5126040830416a8b0
karim <450906k@gmail.com>
2023-01-14 20:32:29 +0500

    getting attackable iislands for user (not right)

--- app/islands.py
+++ app/islands.py
@@ -7,6 +7,17 @@ from app.tables import user, island, unit
 blueprint = Blueprint('islands', __name__)
 
 
+def map_islands(db, cursor, user_id):
+    return list(map(lambda i:
+                    {'id': i.id,
+                     'map_id': i.map_id,
+                     'units': list(
+                         map(lambda un: {'x': un.x, 'y': un.y},
+                             db.session.execute(
+                                 unit.select().where(and_(unit.c.user_id == user_id, unit.c.island_id == i.id))))
+                     )}, cursor))
+
+
 @blueprint.get('/<user_id>/islands')
 def get_user_islands(user_id):
     db = current_app.config['db']
@@ -14,19 +25,18 @@ def get_user_islands(user_id):
     if not db.session.query(exists(user.select().where(user.c.id == u_id))).scalar():
         return 'User not found', 404
     result = db.session.execute(island.select().where(island.c.user_id == u_id))
-    return list(map(lambda i:
-                    {'id': i.id,
-                     'map_id': i.map_id,
-                     'units': list(
-                         map(lambda un: {'x': un.x, 'y': un.y},
-                             db.session.execute(unit.select().where(and_(unit.c.user_id == u_id, unit.c.island_id == i.id))))
-                     )}, result))
+    return map_islands(db, result, u_id)
 
 
 @blueprint.get('/<user_id>/islands/attackable')
 def get_attackable_islands(user_id):
-    pass
-    # return islands.storage.get_attackable_islands(int(user_id))
+    db = current_app.config['db']
+    u_id = int(user_id)
+    current_user = db.session.execute(db.select([user.c.nickname, user.c.id]).where(user.c.id == user_id)).first()
+    if current_user is None:
+        return 'User not found', 404
+    result = db.session.execute(island.select().where(island.c.user_id != u_id))
+    return map_islands(db, result, u_id)
 
 
 @blueprint.get('/<user_id>/islands/<island_id>')
